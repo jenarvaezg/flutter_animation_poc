1fbee9f88607c4d0fdc398891bd8c33ddd6e9b36
José Enrique Narváez Gago <jnarvaez@jooraccess.com>
2019-01-07 19:41:04 +0100

    Finished cat animation

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 import '../widgets/cat.dart';
 
 class Home extends StatefulWidget {
@@ -9,56 +10,145 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> with TickerProviderStateMixin {
   Animation<double> catAnimation;
   AnimationController catController;
+  Animation<double> boxAnimation;
+  AnimationController boxController;
 
   @override
   initState() {
     super.initState();
 
     catController = AnimationController(
-      duration: Duration(seconds: 2),
+      duration: Duration(milliseconds: 200),
       vsync: this,
     );
-
     catAnimation = Tween(
-      begin: 0.0,
-      end: 100.0,
+      begin: -35.0,
+      end: -80.0,
     ).animate(CurvedAnimation(
       parent: catController,
       curve: Curves.easeIn,
     ));
 
+    boxController = AnimationController(
+      duration: Duration(milliseconds: 300),
+      vsync: this,
+    );
+    boxAnimation = Tween(
+      begin: pi * 0.6,
+      end: pi * 0.65,
+    ).animate(CurvedAnimation(
+      parent: boxController,
+      curve: Curves.easeInOut,
+    ));
+    boxController.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        boxController.reverse();
+      } else if (status == AnimationStatus.dismissed) {
+        boxController.forward();
+      }
+    });
+    boxController.forward();
   }
 
   onTap() {
     if (catAnimation.isCompleted) {
+      boxController.forward();
       catController.reverse();
     } else if (catAnimation.isDismissed) {
+      boxController.stop();
       catController.forward();
+
     }
+
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Animation!'),
+      appBar: AppBar(
+        title: Text('Animation!'),
+      ),
+      body: GestureDetector(
+        child: Center(
+          child: Stack(
+            overflow: Overflow.visible,
+            children: <Widget>[
+              buildCatAnimation(),
+              buildBox(),
+              buildLeftFlap(),
+              buildRightFlap(),
+            ],
+          ),
         ),
-        body: GestureDetector(
-          child: buildAnimation(),
-          onTap: onTap,
-        ));
+        onTap: onTap,
+      ),
+    );
   }
 
-  Widget buildAnimation() {
+  Widget buildCatAnimation() {
     return AnimatedBuilder(
       animation: catAnimation,
       builder: (BuildContext context, Widget child) {
-        return Container(
+        return Positioned(
           child: child,
-          margin: EdgeInsets.only(top: catAnimation.value),
+          top: catAnimation.value,
+          right: 0,
+          left: 0,
         );
       },
       child: Cat(),
+    );
+  }
+
+  Widget buildBox() {
+    return Container(
+      height: 200,
+      width: 200,
+      color: Colors.brown,
+    );
+  }
+
+  Widget buildLeftFlap() {
+    return Positioned(
+      left: 10,
+      top: 3,
+      child: AnimatedBuilder(
+        animation: boxAnimation,
+        child: Container(
+          height: 10,
+          width: 125,
+          color: Colors.brown,
+        ),
+        builder: (BuildContext context, Widget child) {
+          return Transform.rotate(
+            child: child,
+            alignment: Alignment.topLeft,
+            angle: boxAnimation.value,
+          );
+        },
+      ),
+    );
+  }
+
+  Widget buildRightFlap() {
+    return Positioned(
+      right: 10,
+      top: 3,
+      child: AnimatedBuilder(
+        animation: boxAnimation,
+        child: Container(
+          height: 10,
+          width: 125,
+          color: Colors.brown,
+        ),
+        builder: (BuildContext context, Widget child) {
+          return Transform.rotate(
+            child: child,
+            alignment: Alignment.topRight,
+            angle: -boxAnimation.value,
+          );
+        },
+      ),
     );
   }
 }
